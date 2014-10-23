845dded92766a6fa88d7543c69a6409bc21c8c67
Junyu Zhan <thedharmabum6@163.com>
2014-10-23 10:08:46 +0800

    Add test title for sign up page

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -10,4 +10,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
   end
+
+  test "signup page title" do
+    get signup_path
+    assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
+  end
+
 end
